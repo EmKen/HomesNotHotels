628283bd6a51a5551682c7e6f377ed531ae8be44
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-04-28 17:27:52 +0800

    Can filter listings by amenity/amenities with checkboxes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def index
-  	if params[:tag]
-  		@listings = Listing.tagged_with(params[:tag])
+  	if params[:keywords]
+  		@selected_tags = params[:keywords]
+  		@listings = Listing.tagged_with(params[:keywords])
   	else
+  		@selected_tags = [nil]
   		@listings = Listing.all
   	end
   end
