c92db093f18373dfcdf46eed9b99fb4c3714e27e
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-04-30 19:40:18 +0800

    Implemented pagination using will paginate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def sortable(field, display)
-		direction = (field == sort_field && sort_direction == "asc" ? "desc" : "asc")
+		direction = (field == sort_field && sort_direction == "ASC" ? "DESC" : "ASC")
 		link_to display, sort: field, direction: direction
 	end
 end
