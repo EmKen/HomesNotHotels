5447466224cfba5eb8d30246cab2f91c1412fd5c
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-05-05 12:20:49 +0800

    Implemented payments with Braintree

---
+++ app/controllers/braintree_controller.rb
@@ -0,0 +1,29 @@
+class BraintreeController < ApplicationController
+  def new
+  	@booking = Booking.find(params[:booking_id])
+  	@listing = @booking.listing
+  	@price = @booking.price(@listing)
+  	@client_token = Braintree::ClientToken.generate
+  end
+
+  def checkout
+	  nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
+	  @booking = Booking.find(params[:checkout_form][:booking_id])
+  	@listing = @booking.listing
+  	price = @booking.price(@listing)
+
+	  result = Braintree::Transaction.sale(
+	   :amount => price,
+	   :payment_method_nonce => nonce_from_the_client,
+	   :options => {
+	      :submit_for_settlement => true
+	    }
+	   )
+
+	  if result.success?
+	    redirect_to :root, :flash => { :success => "Transaction successful!" }
+	  else
+	    redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
+	  end 
+	end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,4 +14,9 @@ class Booking < ApplicationRecord
   def overlap?(x,y)
   (x.check_in - y.check_out) * (y.check_in - x.check_out) > 0
 	end
+
+  def price(listing)
+    days = self.check_out - self.check_in
+    (days * listing.price_per_night).to_i
+  end
 end
